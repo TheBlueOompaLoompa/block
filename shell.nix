5d569c81d4d60a4ec871d7f79aebf89958a65ffb
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2024-03-11 17:15:19 -0500

    Update dev env

--- shell.nix
+++ shell.nix
@@ -1,5 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ cmake pkg-config glew freeglut glfw glm mesa SDL2 SDL2_image freetype glslang zip ];
+    nativeBuildInputs = with pkgs.buildPackages; [
+      cmake
+      pkg-config
+      glew
+      freeglut
+      glfw
+      glm
+      mesa
+      SDL2
+      SDL2_image
+      freetype
+      glslang
+      zip
+      gdb
+    ];
 }
